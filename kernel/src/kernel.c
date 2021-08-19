b297dd94908bb8827820741d03996d3875fdd8b4
corruptmemry <88046785+corruptmemry@users.noreply.github.com>
2021-08-19 21:58:21 +0700

    Update kernel.c

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -8,7 +8,6 @@
 #include <video/gop.h>
 #include <hardware/IO.h>
 #include <debug/panic.h>
-#include <hardware/rtc.h>
 #include <hardware/pci.h>
 #include <stdbool.h>
 typedef struct{
@@ -20,11 +19,8 @@ typedef struct{
 void _initkernel(Framebuffer* framebuffer, PSF1_FONT* psf1_font, kernelData KernelInfo){
     CursorPosition.X = 10; /* Set default cursor position */
     CursorPosition.Y = 10;
-    datetime_t BootTime = rtc_read_datetime(); /* Kernel Startup time */
     Print(framebuffer,psf1_font, 0xFFFFFF, "AirKernel 0.2-indev_beta");
     newline();
-    kprint("Boot Time: ", 0xFFFFFF);
-    kprint(datetime_to_str(&BootTime), 0xFFFFFF);
     newline();
     kprint("[heap] Initializing heap...", 0xFFFFFF);
     newline();
@@ -33,10 +29,7 @@ void _initkernel(Framebuffer* framebuffer, PSF1_FONT* psf1_font, kernelData Kern
     newline();
     mm_init(KernelInfo.kernelEnd);
     pci_init();
-    datetime_t BootTime2 = rtc_read_datetime(); /* Kernel Post-init time */
-    kprint("Loaded in ", 0xFFFFFF);
-    kprint(to_string64(BootTime.second - BootTime2.second), 0xFFFFFF);
-    kprint("s.", 0xFFFFFF);
+    kprint("Loaded.", 0xFFFFFF);
 }
 
 
